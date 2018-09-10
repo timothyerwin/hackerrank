b4365ab3eb8ef01617a9619fb74ffdd85f1ca075
Tim Erwin <tim.erwin@gmail.com>
2018-09-09 23:44:16 -0700

    Create 30-days-of-code-02-operators.js
    https://www.hackerrank.com/challenges/30-operators/problem

---
+++ 30-days-of-code-02-operators.js
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/30-operators/problem
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+    console.log(Math.round(meal_cost + (meal_cost * tip_percent/100) + (meal_cost * tax_percent/100)));
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
