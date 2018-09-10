f0028712840549946a800ade6ba45d1da6a04ecf
Tim Erwin <tim.erwin@gmail.com>
2018-09-09 23:37:55 -0700

    Create 30-days-of-code-01-data-types.js
    https://www.hackerrank.com/challenges/30-data-types/problem

---
+++ 30-days-of-code-01-data-types.js
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/30-data-types/problem
+
+// Declare second integer, double, and String variables.
+let i2, d2, s2;
+
+// Read and save an integer, double, and String to your variables.
+i2 = Number(readLine());
+d2 = Number(readLine());
+s2 = readLine();
+
+// Print the sum of both integer variables on a new line.
+// Print the sum of the double variables on a new line.
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+
+console.log(`${i + i2}\n${(d + d2).toFixed(1)}\n${s + s2}`);
