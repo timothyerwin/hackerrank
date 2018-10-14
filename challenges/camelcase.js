b2c4531a2b4d6d43b8e2a2c2d347fd77c162ea24
Tim Erwin <tim.erwin@gmail.com>
2018-10-13 23:55:24 -0700

    Create camelcase.js

---
+++ challenges/camelcase.js
@@ -0,0 +1,56 @@
+// https://www.hackerrank.com/challenges/camelcase/problem
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the camelcase function below.
+function camelcase(s) {
+  if (!s) {
+    return 0;
+  }
+
+  let count = 1;
+
+  for (let ch of s) {
+    if (ch === ch.toUpperCase()) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = camelcase(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
